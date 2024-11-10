9b6ea23390f0b8441982f8f072b314978d448cae
ulfi julianti <ulfijulianti07@gmail.com>
2024-11-11 00:54:26 +0700

    first commit

--- resources/views/galery/show.blade.php
+++ resources/views/galery/show.blade.php
@@ -6,6 +6,8 @@
     <h2 class="text-3xl font-bold text-center mb-8">{{ $galery->judul }}</h2>git init
     <p class="text-lg text-center text-gray-700 mb-8">{{ $galery->deskripsi }}</p>
     
+
+    
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
         @foreach($galery->photos as $photo)
             <div class="bg-white rounded shadow p-4 flex flex-col items-center">
